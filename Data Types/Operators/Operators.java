1d56e14d60822ba9fed1626e862af54f5a6f86f5
Magarciaaco <49211291+Magarciaaco@users.noreply.github.com>
2020-02-26 18:31:42 -0700

    Add files via upload

---
+++ Data Types/Operators/Operators.java
@@ -0,0 +1,18 @@
+/**
+ * Operators
+ * Coded by: Miguel Angel Garcia Acosta
+ * Contact: MAGA.DevCS@Gmail.com
+ */
+public class Operators {
+    public static void main(String[] args) {
+        int A = 5, B = A;
+        System.out.println("Number A: " + A + "\tNumber B: " + B + "\n");
+        System.out.println("Addition: " + (A + B) );
+        System.out.println("Substraction: " + (A - B) );
+        System.out.println("Multiplication: " + A*B);
+        System.out.println("Division: " + A/B);
+        System.out.println("Modulus: " + A%B);
+        System.out.println("Increment: " + ++A);
+        System.out.println("Decrement: " + --B);
+    }
+}
