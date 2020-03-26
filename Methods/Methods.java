b4c8136f62c83dbcc323486363dced37fb7f6c7f
Magarciaaco <49211291+Magarciaaco@users.noreply.github.com>
2020-03-26 13:21:35 -0600

    Add files via upload

---
+++ Methods/Methods.java
@@ -0,0 +1,17 @@
+/**
+ * Method
+ * Coded by: Miguel Angel Garcia Acosta
+ * Contact: MAGA.DevCS@Gmail.com
+ */
+public class Methods {
+    public static void main(String[] args) {
+        // Hello World From Main
+        System.out.println("Hello World! : This is From Main");
+        // Hello World From Method
+        Method_Name();
+    }
+    // Void Method
+    public static void Method_Name() {
+        System.out.println("Hello World! : This is From Method!");
+    }
+}
